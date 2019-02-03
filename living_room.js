5e1ff47be059b7ab41c40c345e0f65382b03b12d
Michaela <mikimika@gmail.com>
2019-02-02 23:03:13 -0700

    donut eating

--- living_room.js
+++ living_room.js
@@ -1,16 +1,50 @@
 
 var toggleButton = document.querySelector('#button');
 var donutHand = document.querySelector('.donutHand');
+var donutBox = document.querySelector('.donutBox');
 var clickCount = 0;
+var donutCount = 6;
+var iHaveDonut = false;
 
+donutArray= new Array('graphics/Hand1.svg', 'graphics/Hand2.svg', 'graphics/Hand3.svg', 'graphics/Hand4.svg', 'graphics/Hand5.svg');
+
+donutBoxArray = new Array('graphics/0box.svg', 'graphics/1box.svg', 'graphics/2box.svg', 'graphics/3box.svg', 'graphics/4box.svg', 'graphics/5box.svg', 'graphics/6box.svg');
+
+donutBox.onclick = function() {
+	if (clickCount == 0 && iHaveDonut == false){
+		donutCount--;
+		// document.getElementById("donutHand").src=donutArray[clickCount];	
+		if (donutCount <= 0) {
+			donutCount = 6;
+		}
+		document.getElementById("donutB").src=donutBoxArray[donutCount];
+
+		iHaveDonut = true;
+		// clickCount = clickCount + 1;
+	  	donutHand.classList.add('handLeave-active');
+	 	window.setTimeout(updateDonut, 1000);
+	}
+}
 
 donutHand.onclick = function() {
-  clickCount = clickCount + 1;
+	if (donutCount != 6 && iHaveDonut){
+		clickCount = clickCount + 1;
+	  	donutHand.classList.add('handLeave-active');
+	 	window.setTimeout(updateDonut, 1000);
+ 	}
+}
 
-  if (clickCount == 1) {
-    donutHand.classList.add('handLeave-active');
-    window.setTimeout(animationFinished, 5100);
-  }
+function updateDonut(){
+	document.getElementById("donutHand").src=donutArray[clickCount];
+	if (clickCount == donutArray.length - 1){
+  		clickCount = 0;
+  		iHaveDonut = false;
+  	}
+  	window.setTimeout(animationDone, 1000);
+}
+
+function animationDone(){
+	donutHand.classList.remove('handLeave-active');
 }
 
 function play(element){
